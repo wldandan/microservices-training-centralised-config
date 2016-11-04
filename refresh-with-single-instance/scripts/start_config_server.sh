0fa5e7bedc8c1159fd8da321169dfb0d93e7d5ad
Wang lei <wldandan@gmail.com>
2016-11-04 13:59:56 +0800

    Rename single instances refresh

---
+++ refresh-with-single-instance/scripts/start_config_server.sh
@@ -0,0 +1,1 @@
+cd config-server && SERVER_PORT=8020 java -jar build/libs/config-server-0.0.1.jar
---
+++ refresh-with-single-instance/scripts/start_service.sh
@@ -0,0 +1,3 @@
+cd event-service &&
+SERVER_PORT=8080 java -jar build/libs/event-service-0.0.1.jar &&
+SERVER_PORT=8090 java -jar build/libs/event-service-0.0.1.jar
